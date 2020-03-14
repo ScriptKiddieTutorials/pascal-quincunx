1746094ce313ce0dda7a074eb5569211efa306be
ScriptKiddieTutorials <60780745+ScriptKiddieTutorials@users.noreply.github.com>
2020-03-14 11:31:33 +0800

    Add files via upload
    added powershell version beta

---
+++ quincunx.ps1
@@ -0,0 +1,22 @@
+﻿$times = [int32](Read-Host -Prompt 'Enter number') - 1
+$hash = [ordered]@{}
+0..$times | % { $hash += @{"col$_" = 0} }
+
+Write-Host "Ctrl-C to stop"
+
+try
+{
+    while ($true){
+        $sum = 0
+        0..$times | % { $sum += Get-Random 2 }
+        $hash["col$sum"] += 1
+    }
+}
+
+finally
+{
+    ForEach($key in $hash.keys){
+        Write-Host $hash[$key]
+    }
+    pause
+}
